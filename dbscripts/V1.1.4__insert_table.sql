f50617503ae7a71376f0384c06cd38ea71c4fc0c
ashimachanpuriya <124254677+ashimachanpuriya@users.noreply.github.com>
2023-02-09 13:30:13 +0530

    Add files via upload

---
+++ dbscripts/V1.1.4__insert_table.sql
@@ -0,0 +1,7 @@
+USE SCHEMA DEMO.PUBLIC;
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (2, 'Erichsen', 'wxy', 'abc2','Norway');
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (2, 'Ashima', 'Chanpuriya', 'abc','xyz');
---
+++ dbscripts/V1.1.5__insert_table.sql
@@ -0,0 +1,7 @@
+USE SCHEMA DEMO.PUBLIC;
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (3, 'qwer', 'abc', 'abc3','Norway');
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (4, 'Ashi', 'neha', 'abc3','xyz1');
