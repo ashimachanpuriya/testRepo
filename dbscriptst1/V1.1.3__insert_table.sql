158ccc3f06ef5f8377f68a6c3bbeca04b495c44c
ashimachanpuriya <chanpuriyaashima@gmail.com>
2023-02-17 14:00:26 +0530

    adding

---
+++ dbscriptst1/V1.1.3__insert_table.sql
@@ -0,0 +1,4 @@
+USE SCHEMA ABC.PUBLIC;
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (1, 'Tom B. Erichsen', 'Skagen', 'Stavanger','Norway');
---
+++ dbscriptst2/V1.1.4__insert_table.sql
@@ -0,0 +1,10 @@
+USE SCHEMA ABC.PUBLIC;
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (2, 'Erichsen', 'wxy', 'abc2','Norway');
+
+INSERT INTO emp(PersonID , LastName , FirstName, Address , City)
+VALUES (3, 'Ashima', 'Chanpuriya', 'abc','xyz');
+
+INSERT INTO Persons (PersonID , LastName , FirstName, Address , City)
+VALUES (1, 'Ashima', 'Chanpuriya', 'abc','xyz');
